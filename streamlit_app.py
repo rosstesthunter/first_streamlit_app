c5ef9245197215716be7584e94691e3eacfa0dde
rosstesthunter <122835797+rosstesthunter@users.noreply.github.com>
2023-01-26 13:56:13 -0700

    Update streamlit_app.py
    Capital O for Oatmeal

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu:')
-streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket smoothie')
 streamlit.text('Hard-boiled free-range egg!')
