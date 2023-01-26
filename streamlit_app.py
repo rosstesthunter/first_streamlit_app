4975584959d7e326a59084134f14d982b66d9b13
rosstesthunter <122835797+rosstesthunter@users.noreply.github.com>
2023-01-26 14:11:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Favourites')
+streamlit.header('Breakfast Favourites Beaming face with smiling eyes')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket smoothie')
 streamlit.text('Hard-boiled free-range egg')
